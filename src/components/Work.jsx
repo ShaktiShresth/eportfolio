4211fc26f5df858e17d3fb995f3241bff281a5bf
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2023-07-10 10:26:45 +0545

    Works updated - car game

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,6 +4,7 @@ import Inotebook from '../assets/inotebookimg.png';
 import BookCart from '../assets/bookscartimg.png';
 import Portfolio from '../assets/portfoliosample.png';
 import Dribbledesign from "../assets/dribbledesign.png"
+import CarRace from '../assets/cargameimg.png'
 
 const Work = () => {
   return (
@@ -99,9 +100,26 @@ const Work = () => {
                     </div>
                 </div>
             </div>
-            <div style={{backgroundImage: `url(${Dribbledesign})`}}  className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+            <div style={{backgroundImage: `url(${CarRace})`}}  className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
                 {/* Hover Effects */}
                 <div className='opacity-0 group-hover:opacity-100'>
+                    <span className='text-2xl font-bold text-white tracking-wider'>
+                        Javascript Car Game
+                    </span>
+                    <div className='pt-8 text-center'>
+                        <a href="https://shaktishresth.github.io/car-racing-game-JS//" target="_blank" rel="noreferrer">
+                            <button className='text-center rounded-lg px-4 py-3 hover:text-pink-600'>Demo</button>
+                        </a>
+                        <a href="https://github.com/ShaktiShresth/car-racing-game-JS" target="_blank" rel="noreferrer">
+                            <button className='text-center rounded-lg px-4 py-3 hover:text-pink-600'>Code</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            {/* <div style={{backgroundImage: `url(${Dribbledesign})`}}  className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"> */}
+                {/* Hover Effects */}
+                {/* <div className='opacity-0 group-hover:opacity-100'>
                     <span className='text-2xl font-bold text-white tracking-wider'>
                         HTML CSS design
                     </span>
@@ -113,8 +131,8 @@ const Work = () => {
                             <button className='text-center rounded-lg px-4 py-3 hover:text-pink-600'>Code</button>
                         </a>
                     </div>
-                </div>
-            </div>
+                </div> */}
+            {/* </div>  */}
         </div>
       </div>
     </div>
